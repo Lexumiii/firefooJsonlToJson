df6cb3c4f307a5ed99c70caa343bec2eb510a701
= <=>
2022-09-19 21:12:23 +0200

    Increased performance | Updated readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jsonl to Json converter
+This tool can convert jsonl files from a firebase export (with firefoo) to regular object based json file. 
+The input file must be called "json.jsonl".
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class Converter:
         print("--- Preparing data ---")
         self.file_name = "./file.jsonl"
         self.json_file_name = "./json_file.json"
-        self.converted_output_file_name = "./output.json"
         # check if files exist
         file_exists = os.path.exists(self.json_file_name)
 
@@ -18,15 +17,6 @@ class Converter:
             # file does not exist -> create
             with open(self.json_file_name, 'w') as f:
                 f.write('{}')
-
-         # check if files exist
-        file_exists = os.path.exists(self.converted_output_file_name)
-
-        if(file_exists == False):
-            # file does not exist -> create
-            with open(self.converted_output_file_name, 'w') as f:
-                f.write('{}')
-
         self.unicode_escape = re.compile(
             r'(?<!\\)'
             r'(?:\\u([dD][89abAB][a-fA-F0-9]{2})\\u([dD][c-fC-F][a-fA-F0-9]{2})'
@@ -46,63 +36,57 @@ class Converter:
         start_time = datetime.datetime.now()
         print("Start: ", start_time, "\n")
         print("--- Starting conversion ---")
-        for line in progressbar(self.file_content, "Computing: ", 40):
-            
+        # read data
+        with open(self.json_file_name, "r+", encoding="utf-8") as file:
+            file_data = json.load(file)
+            file.close()
+        for line in progressbar(self.file_content, "Converting: ", 40):
             # loop over keys
             for key in line:
                 # skip if unwanted keys
                 if(key.startswith("__") or key == "__id__"):
                     continue
 
-                path = line["__path__"].split("/") # get path as object
+                path = line["__path__"].split("/")  # get path as array
                 value = line[key]
 
-                # read data
-                with open(self.json_file_name, "r+", encoding="utf-8") as file:
-                    file_data = json.load(file)
-                    file.close()
+                if path[0] not in file_data:
+                    file_data[path[0]] = {}  # add empty main collection object
 
-                # write data
-                with open(self.json_file_name, "w+", encoding="utf-8") as file:
-                    if path[0] not in file_data:
-                        file_data[path[0]] = {} # add empty main collection object
-                        
-                    if path[1] not in file_data[path[0]]:
-                        file_data[path[0]][path[1]] = {} # add empty document object to main collection
-                        
-                    if(len(path) == 2):
-                        file_data[path[0]][path[1]][key] = value # add key and value if no subcollection is there
-                    else:
-                        # has subcollection
-                        collectionName = path[2] # get subcollection name
-                        documentId = path[3] # get current document id of subcollection
-                        
-                        # check if subcollection already exists in dictionary
-                        if collectionName not in file_data[path[0]][path[1]]:
-                            file_data[path[0]][path[1]][collectionName] = {} # add empty subcollection if 
-                            
-                        if documentId not in file_data[path[0]][path[1]][collectionName]:
-                            file_data[path[0]][path[1]][collectionName][documentId] = {} # add empty object if subcollection has no document
-                        
-                        file_data[path[0]][path[1]][collectionName][documentId][key] = value # add key and value 
+                if path[1] not in file_data[path[0]]:
+                    # add empty document object to main collection
+                    file_data[path[0]][path[1]] = {}
 
-                    file.seek(0)
-                    json.dump(file_data, file, ensure_ascii=False)
-                    file.close()
+                if(len(path) == 2):
+                    # add key and value if no subcollection is there
+                    file_data[path[0]][path[1]][key] = value
+                else:
+                    # has subcollection
+                    collectionName = path[2]  # get subcollection name
+                    # get current document id of subcollection
+                    documentId = path[3]
+
+                    # check if subcollection already exists in dictionary
+                    if collectionName not in file_data[path[0]][path[1]]:
+                        # add empty subcollection if
+                        file_data[path[0]][path[1]][collectionName] = {}
+
+                    if documentId not in file_data[path[0]][path[1]][collectionName]:
+                        # add empty object if subcollection has no document
+                        file_data[path[0]][path[1]][collectionName][documentId] = {}
+
+                    # add key and value
+                    file_data[path[0]][path[1]][collectionName][documentId][key] = value
+
+        # write data
+        with open(self.json_file_name, "w+", encoding="utf-8") as file:
+            file.seek(0)
+            json.dump(file_data, file, ensure_ascii=False)
+            file.close()
         print("--- Finished conversion ---\n")
-
-        print("--- Starting conversion of unicode in json ---")
-        with open(self.json_file_name, "r", encoding="utf-8") as input:
-            with open(self.converted_output_file_name, "w", encoding="utf-8") as output:
-                for line in input:
-                    output.write(self.unicode_escape.sub(self.replace, line))
-        print("--- Finished conversion of unicode in json ---")
-
         end_time = datetime.datetime.now()
 
         print("End: ", end_time)
-
-        print("INFO:  This programm created 2 files: 'output.json' and 'json_file.json. The output.json has an additional layer of unicode characters conversion")
 
         # calculate time run
         difference = end_time - start_time
@@ -112,7 +96,7 @@ class Converter:
         return bytes.fromhex(''.join(m.groups(''))).decode('utf-16-be')
 
 
-def progressbar(it, prefix="", size=60, out=sys.stdout):  # Python3.3+
+def progressbar(it, prefix="", size=60, out=sys.stdout):
     count = len(it)
 
     def show(j):
