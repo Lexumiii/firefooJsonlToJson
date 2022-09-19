82f5df2713ff56f95c23846b6cb44643b547f757
= <=>
2022-09-19 07:10:10 +0200

    Fixed main collection not getting subcollections bug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+from datetime import datetime
 
 class Converter:
     def __init__(self):
@@ -7,16 +7,21 @@ class Converter:
         self.json_file_name = "./json_file.json"
 
         # open jsonl file
-        with open(self.file_name, "r+") as f:
+        with open(self.file_name, "r+", encoding="utf8") as f:
             data = [json.loads(line) for line in f]
             self.file_content = data
 
     def start(self):
+        # print start
+        start_time = datetime.now()
+        print("Start: ", start_time)
+        
         # loop over all lines
         for line in self.file_content:
             id = line["__id__"]
             # loop over keys
             for key in line:
+                print("key:", key)
                 # skip if __
                 if(key.startswith("__") or key == "__id__"):
                     continue
@@ -24,22 +29,19 @@ class Converter:
                 path = line["__path__"].split("/")
                 value = line[key]
                 # add to json file
-
+    
                 # read data
-                with open(self.json_file_name, "r+") as file:
+                with open(self.json_file_name, "r+", encoding="utf8") as file:
                     file_data = json.load(file)
                     file.close()
 
                 # write data
-                with open(self.json_file_name, "w+") as file:
+                with open(self.json_file_name, "w+", encoding="utf8") as file:
                     if path[0] not in file_data:
                         file_data[path[0]] = {}
+                    if path[1] not in file_data[path[0]]:
                         file_data[path[0]][path[1]] = {}
-                        file_data[path[0]][path[1]][key] = value
-                    else:
-                        # create document id
-                        file_data[path[0]][path[1]] = {}
-                        file_data[path[0]][path[1]].update({key: value})
+                    file_data[path[0]][path[1]][key] = value
 
                     file.seek(0)
                     json.dump(file_data, file)
@@ -49,53 +51,33 @@ class Converter:
                     # one sub collection
                     collectionName = path[2]
                     documentId = path[3]
-                    print("1 subcollection", collectionName, documentId)
 
                     # read data
-                    with open(self.json_file_name, "r+") as file:
+                    with open(self.json_file_name, "r+", encoding="utf8") as file:
                         file_data = json.load(file)
                         file.close()
 
                     # write data
-                    with open(self.json_file_name, "w+") as file:
-                        print(collectionName, documentId)
+                    with open(self.json_file_name, "w+", encoding="utf8") as file:
                         if collectionName not in file_data:
-                            file_data[path[0]][collectionName] = {}
-                            file_data[path[0]][collectionName][documentId] = {}
-                            file_data[path[0]][collectionName][documentId][key] = value
-                        else:
-                            # create document id
                             file_data[path[0]][path[1]][collectionName] = {}
+                        if documentId not in file_data[path[0]][path[1]][collectionName]:
                             file_data[path[0]][path[1]][collectionName][documentId] = {}
-                            file_data[path[0]][path[1]][collectionName][documentId].update({key: value})
+                        file_data[path[0]][path[1]][collectionName][documentId][key] = value
 
                         file.seek(0)
                         json.dump(file_data, file)
                         file.close()
                     pass
-
-    def writeJsonFile(self, path: str, key: str, value: str, documentId: str):
-
-        # read data
-        with open(self.json_file_name, "r+") as file:
-            file_data = json.load(file)
-            file.close()
-
-        # write data
-        with open(self.json_file_name, "w+") as file:
-            if path not in file_data:
-                file_data[path] = {}
-                file_data[path][documentId] = {}
-                file_data[path][documentId][key] = value
-            else:
-                # create document id
-                file_data[path][documentId] = {}
-                file_data[path][documentId].update({key: value})
-
-            file.seek(0)
-            json.dump(file_data, file)
-            file.close()
-
+        end_time = datetime.now()        
+        print("End: ", datetime.now())
+        
+        # calculate time run
+        difference = start_time - end_time
+        seconds_in_day = 24 * 60 * 60
+        res = divmod(difference.days * seconds_in_day + difference.seconds, 60)
+        print("time run:", res[0], "hours", res[1], "minutes")
+        
     def pairwise(self, iterable):
         a = iter(iterable)
         return zip(a, a)
@@ -103,3 +85,4 @@ class Converter:
 
 if __name__ == "__main__":
     Converter().start()
+
